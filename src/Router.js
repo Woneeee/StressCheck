050b5720444819b9182b42e68477716a641f4da5
Woneeee <a6885088@naver.com>
2024-09-23 14:10:41 +0900

    components setting

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,21 @@
+import { HashRouter, Route, Routes } from "react-router-dom";
+import { routes } from "./routes";
+import { Home } from "./pages/home/Home";
+import { QnA } from "./pages/qna/QnA";
+import { Result } from "./pages/result/Result";
+import { PageNotFound } from "./pages/PageNotFound";
+
 const Router = () => {
-  return <div>Router</div>;
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path={routes.home} element={<Home />} />
+        <Route path={routes.qna} element={<QnA />} />
+        <Route path={routes.result} element={<Result />} />
+        <Route path="/*" element={<PageNotFound />} />
+      </Routes>
+    </HashRouter>
+  );
 };
 
 export default Router;
